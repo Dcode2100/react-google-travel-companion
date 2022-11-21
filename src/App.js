6416b52fb2a604f075f114995c67907de0d65a6b
Dikshant Ashok Solanki <dcode2100@gmail.com>
2022-11-21 12:18:08 +0530

    Headercomp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
+import {Header,List,Map} from "../src/components/index";
+
+const App = () => {
   return (
-    <div> 
-        
-    </div>
-  );
+    <>
+      <CssBaseline/>
+      <Header/>
+      <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs = {12} md={4} >
+            <List/>
+          </Grid>
+        <Grid item xs = {12} md={4} >
+            <Map/>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import googleMapReact from 'google-map-react';
+import {  Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+
+const Map = () => {
+  return (
+    <div>Map</div>
+  )
+}
+
+export default Map
